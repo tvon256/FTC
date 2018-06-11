ee13496bcbd30c062bfc835c7a983b55c6c4186f
Thomas <thholten@live.com>
2018-06-10 21:45:29 -0400

    Modify sample Opmode to take hardware class

--- BasicOpMode_Iterative.java
+++ BasicOpMode_Iterative.java
@@ -55,28 +55,17 @@ public class BasicOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+
+    //Allow methods and variables from hardware class to be accessed
+    Hardware robot = new Hardware();
 
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        // Tell the driver that initialization is complete.
+        //initialize hardware from hardware class (note: hardwareMap comes from extended class OpMode and stores hardware config from phone)
+        robot.init(hardwareMap);
         telemetry.addData("Status", "Initialized");
     }
 
@@ -120,8 +109,8 @@ public class BasicOpMode_Iterative extends OpMode
         // rightPower = -gamepad1.right_stick_y ;
 
         // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
+        robot.leftDrive.setPower(leftPower);
+        robot.rightDrive.setPower(rightPower);
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
